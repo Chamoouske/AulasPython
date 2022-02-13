fe9ad0393f131ba5d0f1a6eded5be3558504e78e
Ajax Lima <ajaxlima94@gmail.com>
2022-02-13 18:20:55 -0300

    teste da biblioteca platform

---
+++ teste_platform.py
@@ -0,0 +1,14 @@
+# import platform
+import getpass
+# from datetime import datetime
+
+# print("Nome da máquina:  ", platform.node())
+# print("Arquitetura:      ", platform.architecture())
+# print("SO:               ", platform.system())
+# print("Versão do SO:     ", platform.release())
+# print("Processador:      ", platform.processor())
+# print("versão Python:    ", platform.python_version())
+
+# print(datetime.now())
+
+print(getpass.getuser())
